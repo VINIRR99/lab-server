e33afbf6be6a27ec2dd21116cf7fea7d424b9f71
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-10-31 11:24:05 -0400

    express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hi there guy');
+import "dotenv/config";
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(express.static('public'));
+
+app.get('/', (req: Request, res: Response): void => {res.sendFile(process.cwd() + '/views/home.html')});
+
+app.listen(process.env.PORT, (): void => {console.log(`App is listening on port ${process.env.PORT}`)});
